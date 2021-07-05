286805408b372bd188a49e3e1b58b5fb195be545
SupaMaggie70Incorporated <magnus.larsson.mn@gmail.com>
2021-07-04 19:54:33 -0500

    hmm

--- randomLetterGenerator.py
+++ randomLetterGenerator.py
@@ -1,6 +1,5 @@
 import random
-def randomLetter(excludedList = []):
-    global letter
+def randomLetter(excludedList = [],capital = False):
     excludedList = list(dict.fromkeys(excludedList))
     alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     for listLetter in excludedList:
@@ -12,9 +11,11 @@ def randomLetter(excludedList = []):
             alphabet.remove(listLetter)
         except ValueError:
             pass
-    return(random.choice(alphabet))
-def randomLetters(excludedList = [],listLength = 1):
-    global letter
+    if not capital:
+        return(random.choice(alphabet))
+    else:
+        return((random.choice(alphabet)).upper)
+def randomLetters(excludedList = [],listLength = 1,capital = False):
     excludedList = list(dict.fromkeys(excludedList))
     alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     for listLetter in excludedList:
@@ -25,6 +26,32 @@ def randomLetters(excludedList = [],listLength = 1):
         try:
             alphabet.remove(listLetter)
         except ValueError:
+            pass
+    returnedValue = (random.choices(alphabet,k=listLength))
+    if capital:
+        for i in range(len(returnedValue)):
             pass
     return(random.choices(alphabet,k=listLength))
-print(randomLetters(listLength=3))
+def noRepRandomLetters(excludedList = [],listLength = 1,capital = False):
+    returnedValue = []
+    excludedList = list(dict.fromkeys(excludedList))
+    alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    for listLetter in excludedList:
+        letterIndex = excludedList.index(listLetter)
+        listLetter = listLetter.lower()
+        excludedList[letterIndex] = listLetter
+    for listLetter in excludedList:
+        try:
+            alphabet.remove(listLetter)
+        except ValueError:
+            pass
+    for i in range(listLength):
+        letterToAdd = random.choice(alphabet)
+        alphabet.remove(letterToAdd)
+        if not capital:
+            returnedValue.append(letterToAdd)
+        elif capital:
+            returnedValue.append((letterToAdd).upper)
+    print(len(returnedValue))
+    return(returnedValue)
+print(noRepRandomLetters(listLength=26))
